88d93848ef76cce85b80757b4ab1515f383af02f
makeihany <makeihany@gmail.com>
2020-06-28 20:35:51 +0430

    First commit

---
+++ blog/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    date = models.DateField()
+    #created_at = models.DateTimeField(auto_now_add=True)
+    #updated_at = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return self.title
